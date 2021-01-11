f293b51d2b91db54596c081c9eef4cbdc007bd60
Paulo Henrique <paulo.henrique06@hotmail.com>
2021-01-11 20:58:50 -0300

    lab02 módulo 03

--- PythonFundamentos/Cap03/Lab02/calculadora_v1.py
+++ PythonFundamentos/Cap03/Lab02/calculadora_v1.py
@@ -1,9 +1,23 @@
 # Calculadora em Python
 
-# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos 2 e 3. 
+# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos 2 e 3.
 # A solução será apresentada no próximo capítulo!
 # Assista o vídeo com a execução do programa!
 
 print("\n******************* Python Calculator *******************")
+print('Selecione o número da operação desejada:\n 1- Soma\n 2- Subtração\n 3- Multiplicação\n 4- Divisão\n')
+opcao = int(input('Digite sua opção (1/2/3/4): '))
+num1 = int(input('Digite o 1o número: '))
+num2 = int(input('Digite o 2o número: '))
+
+def operacao(opcao):
+  if(opcao == 1): res = num1 + num2
+  elif(opcao == 2): res = num1 - num2
+  elif(opcao == 3): res = num1 * num2
+  elif(opcao == 4): res = num1 / num2
+  return res
+
+print(operacao(opcao))
+
 
 
